cf1fc1e0d2fe0c80931d7e6a13075fdaa7581940
Bo-Deng <boyang.deng@gmail.com>
2017-09-04 10:46:33 -0500

    Setup ftc sdk

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrollbotAuto.java
@@ -0,0 +1,13 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+
+
+/**
+ * Created by Bo on 9/4/2017.
+ */
+public class TrollbotAuto extends LinearOpMode {
+    public void runOpMode() {
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrollbotTeleOp.java
@@ -0,0 +1,17 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+/**
+ * Created by Bo on 9/4/2017.
+ */
+public class TrollbotTeleOp extends OpMode {
+
+    public void init() {
+
+    }
+
+    public void loop() {
+
+    }
+}
