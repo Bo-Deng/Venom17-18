de792399d0db3c1a133619517a4d99f6e1b12b47
Hamza A <hamzaimran2011@gmail.com>
2017-09-12 16:16:58 -0500

    labelled Ryan's code

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrollbotTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrollbotTeleOp.java
@@ -22,7 +22,6 @@ public class TrollbotTeleOp extends OpMode {
         motorBR = hardwareMap.dcMotor.get("motorBR");
         motorFR.setDirection(DcMotorSimple.Direction.REVERSE);
         motorBR.setDirection(DcMotorSimple.Direction.REVERSE);
-
     }
 
     public void loop() {
@@ -32,16 +31,19 @@ public class TrollbotTeleOp extends OpMode {
 
             startMotor(gamepad1.left_stick_y);
         }
+
         // if statement for left/right
-        if (Math.abs(gamepad1.left_stick_y) < .1 && Math.abs(gamepad1.left_stick_x) > .1)
-        {
+        if (Math.abs(gamepad1.left_stick_y) < .1 && Math.abs(gamepad1.left_stick_x) > .1) {
+
             motorFR.setPower(gamepad1.left_stick_x);
             motorBR.setPower(-gamepad1.left_stick_x);
             motorFL.setPower(-gamepad1.left_stick_x);
             motorBL.setPower(gamepad1.left_stick_x);
         }
-        if (Math.abs(gamepad1.left_stick_y) > .1 && Math.abs(gamepad1.left_stick_x) > .1)
-        {
+
+        // if statement for non-cardinal directions
+        if (Math.abs(gamepad1.left_stick_y) > .1 && Math.abs(gamepad1.left_stick_x) > .1) {
+
             double x = gamepad1.left_stick_y;
             double y = gamepad1.left_stick_x;
             double r = gamepad1.right_stick_x;
@@ -51,22 +53,21 @@ public class TrollbotTeleOp extends OpMode {
             motorBL.setPower((x - y + r) / max);
             motorBR.setPower((x + y - r) / max);
         }
+
         else
             stopMotor();
-
-
     }
 
     public void startMotor(double Speed) {
+
         motorFL.setPower(-Speed);
         motorBL.setPower(-Speed);
         motorFR.setPower(Speed);
         motorBR.setPower(Speed);
     }
 
-
-
     public void stopMotor() {
+
         motorFL.setPower(0);
         motorFR.setPower(0);
         motorBL.setPower(0);
