eb92cef134849db21a5ca710066d25c093f6fc8e
viperbots <31671851+viperbots@users.noreply.github.com>
2017-09-12 07:51:46 -0500

    Wrote the teleop code for the left joy stick mechnum wheels

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrollbotTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrollbotTeleOp.java
@@ -27,35 +27,44 @@ public class TrollbotTeleOp extends OpMode {
 
     public void loop() {
 
-        if (Math.abs(gamepad1.left_stick_y) > .1) {
+        // if statement for forward/backward
+        if (Math.abs(gamepad1.left_stick_y) > .1 && Math.abs(gamepad1.left_stick_x) < .1) {
 
-            startLMotor(gamepad1.left_stick_y);
+            startMotor(gamepad1.left_stick_y);
         }
-        else {
-            motorBL.setPower(0);
-            motorFL.setPower(0);
+        // if statement for left/right
+        if (Math.abs(gamepad1.left_stick_y) < .1 && Math.abs(gamepad1.left_stick_x) > .1)
+        {
+            motorFR.setPower(gamepad1.left_stick_x);
+            motorBR.setPower(-gamepad1.left_stick_x);
+            motorFL.setPower(-gamepad1.left_stick_x);
+            motorBL.setPower(gamepad1.left_stick_x);
         }
+        if (Math.abs(gamepad1.left_stick_y) > .1 && Math.abs(gamepad1.left_stick_x) > .1)
+        {
+            double x = gamepad1.left_stick_x;
+            double y = gamepad1.left_stick_y;
+            double r = Math.pow(Math.pow(x,2) + Math.pow(y ,2), 1/2);
+            double max = Math.abs(Math.max(Math.abs(r), Math.max(Math.abs(x), Math.abs(y))));
+            motorFL.setPower((x + y + r) / max);
+            motorFR.setPower((x - y - r) / max);
+            motorBL.setPower((x - y + r) / max);
+            motorBR.setPower((x + y - r) / max);
+        }
+        else
+            stopMotor();
 
-        if (Math.abs(gamepad1.right_stick_y) > .1) {
-
-            startRMotor(gamepad1.right_stick_y);
-        }
-        else {
-            motorBR.setPower(0);
-            motorFR.setPower(0);
-        }
 
     }
 
-    public void startLMotor(double lSpeed) {
-        motorFL.setPower(lSpeed);
-        motorBL.setPower(lSpeed);
+    public void startMotor(double Speed) {
+        motorFL.setPower(-Speed);
+        motorBL.setPower(-Speed);
+        motorFR.setPower(Speed);
+        motorBR.setPower(Speed);
     }
 
-    public void startRMotor(double rSpeed) {
-        motorFL.setPower(rSpeed);
-        motorBL.setPower(rSpeed);
-    }
+
 
     public void stopMotor() {
         motorFL.setPower(0);
