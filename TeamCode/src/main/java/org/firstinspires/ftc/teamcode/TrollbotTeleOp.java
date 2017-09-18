08ef12ad488a80c71dbb4c670d416f78757fc6e4
viperbots <31671851+viperbots@users.noreply.github.com>
2017-09-18 07:52:35 -0500

    Changed the code to drive train

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrollbotTeleOp.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/TrollbotTeleOp.java
@@ -23,8 +23,8 @@ public class TrollbotTeleOp extends OpMode {
         motorFR = hardwareMap.dcMotor.get("motorFR");
         motorBL = hardwareMap.dcMotor.get("motorBL");
         motorBR = hardwareMap.dcMotor.get("motorBR");
-        motorFL.setDirection(DcMotorSimple.Direction.REVERSE);
-        motorBL.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorFR.setDirection(DcMotorSimple.Direction.REVERSE);
+        motorBR.setDirection(DcMotorSimple.Direction.REVERSE);
 
         telemetry.addData("init ", "completed");
         telemetry.update();
@@ -45,7 +45,7 @@ public class TrollbotTeleOp extends OpMode {
             motorFL.setPower(-gamepad1.left_stick_x);
             motorBL.setPower(gamepad1.left_stick_x);
         }*/
-        if (Math.abs(gamepad1.left_stick_y) > .1 || Math.abs(gamepad1.left_stick_x) > .1 || Math.abs(gamepad1.right_stick_x) > .1)
+        /*// if (Math.abs(gamepad1.left_stick_y) > .1 || Math.abs(gamepad1.left_stick_x) > .1 || Math.abs(gamepad1.right_stick_x) > .1)
         {
             double x = Math.abs(gamepad1.left_stick_y) > .1 ? gamepad1.left_stick_y : 0;
             double y = Math.abs(gamepad1.left_stick_x) > .1 ? gamepad1.left_stick_x : 0;
@@ -61,7 +61,7 @@ public class TrollbotTeleOp extends OpMode {
         }
         else
             stopMotor();
-
+*/
         if (gamepad1.a)
             motorBL.setPower(1);
         if (gamepad1.b)
@@ -70,6 +70,41 @@ public class TrollbotTeleOp extends OpMode {
             motorFL.setPower(1);
         if (gamepad1.x)
             motorFR.setPower(1);
+
+
+        double yL = gamepad1.left_stick_y;
+        double yR = gamepad1.right_stick_y;
+        double rt = gamepad1.right_trigger;
+        double lt = gamepad1.left_trigger;
+        double r = Math.max(Math.abs(rt), Math.abs(lt));
+        /*// double max = Math.max(Math.abs(x + y + r), Math.max(Math.abs(x - y - r), Math.max(Math.abs(x - y + r), Math.abs(x + y - r))));
+        if (max < 1)
+            max = 1;
+
+        motorFL.setPower((x - y - r) / max);
+        motorFR.setPower((x + y + r) / max);
+        motorBL.setPower((x + y - r) / max);
+        motorBR.setPower((x - y + r) / max); */
+        if (Math.abs(yL) > .1 || Math.abs(yR) > .1) {
+            motorBL.setPower(-yL);
+            motorFL.setPower(-yL);
+            motorBR.setPower(-yR);
+            motorFR.setPower(-yR);
+        }
+        else if (rt != 0){
+            motorBL.setPower(-rt);
+            motorFL.setPower(rt);
+            motorBR.setPower(rt);
+            motorFR.setPower(-rt);
+        }
+        else if (lt != 0){
+            motorBL.setPower(lt);
+            motorFL.setPower(-lt);
+            motorBR.setPower(-lt);
+            motorFR.setPower(lt);
+        }
+        else
+            stopMotor();
     }
 
     public void startMotor(double Speed) {
