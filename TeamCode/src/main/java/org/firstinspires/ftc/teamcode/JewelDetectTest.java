d6fddf4779d92c93849d29e41e295289871b9a09
Hamza A <hamzaimran2011@gmail.com>
2017-10-10 20:04:48 -0500

    Small changes to Jewel Detect Test
    Also added comments to clarify what code is doing.

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JewelDetectTest.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/JewelDetectTest.java
@@ -6,6 +6,9 @@ import com.qualcomm.robotcore.eventloop.opmode.Autonomous;
 
 import for_camera_opmodes.OpModeCamera;
 
+import org.opencv.android.InstallCallbackInterface;
+import org.opencv.android.LoaderCallbackInterface;
+import org.opencv.android.OpenCVLoader;
 import org.opencv.android.Utils;
 import org.opencv.core.CvType;
 import org.opencv.core.Mat;
@@ -13,56 +16,77 @@ import org.opencv.core.Size;
 import org.opencv.imgcodecs.Imgcodecs;
 import org.opencv.imgproc.Imgproc;
 import org.opencv.objdetect.CascadeClassifier;
+import org.opencv.core.Core;
+
 
 /**
  * Created by Bo on 9/30/2017.
  */
 
-@Autonomous(name = "CircleTest", group = "autonomous")
+@Autonomous (name = "JewelDetect", group = "test")
 public class JewelDetectTest extends OpModeCamera {
 
-    public Mat myMat;
+    //Object Declarations
+    public Mat imgMat; //Mat = matrix
     public Mat CMat;
-    public Imgcodecs myCodec;
-    public CascadeClassifier myCas;
-    public Imgproc myProc;
+    public Imgproc myProc; //Imgproc = Image Processing Object (used to identify circles/modify image)
 
-    private double dp = 1.0;
+    private double dp = 1.0; //variables used to locate circles in image
     private double minDst = 5.0;
+
+    public OpenCVLoader openCVLoader; //loads the opencv library
 
     public void init() {
 
-        /* setCameraDownsampling(2); //down sampling = lower resolution for higher speed; TEST THIS
+        telemetry.addData("loading", "libs");
+        telemetry.update();
+        System.loadLibrary("opencv_java"); //loads opencv libs
+        telemetry.addData("loading", "complete");
+        telemetry.update();
 
-        myCas = new CascadeClassifier();
-        myCodec = new Imgcodecs();
-        myMat = new Mat();
-        myProc = new Imgproc(); */
+        setCameraDownsampling(2); //down sampling = lower resolution for higher speed; TEST THIS (may be unnecessary)
 
-        startCamera();
+        myProc = new Imgproc(); //initializations
+        openCVLoader = new OpenCVLoader();
+
+        startCamera(); //camera init
         telemetry.addData("Camera", "Initialized");
+        telemetry.update();
+
+        if (openCVLoader.initDebug()) { //inits opencv libs
+            telemetry.addData("OpenCV Library", "Found");
+        }
+        else {
+            telemetry.addData("OpenCV Library", "Not Found");
+        }
         telemetry.update();
 
     }
 
     public void loop() {
 
-        if (imageReady()) {
+        if (imageReady()) { //when image received from camera
 
-            Bitmap img;
+            Bitmap img; //creates a bitmap of the image
+
             img = convertYuvImageToRgb(yuvImage, width, height, 1);
-            Utils.bitmapToMat(img, myMat);
 
-            myProc.blur(myMat, myMat, new Size(width, height));
+            //program crashes here
+            //imgMat = new Mat();
+            imgMat = new Mat(new Size(img.getWidth(), img.getHeight()), CvType.CV_8UC1); //supposed to put image in matrix
 
-            CMat = new Mat(myMat.size(), CvType.CV_8UC1);
+            Utils.bitmapToMat(img, imgMat);
+
+            myProc.blur(imgMat, imgMat, new Size(width, height)); //blurs image to eliminate false circles
+
+            CMat = new Mat(imgMat.size(), CvType.CV_8UC1); //creates new matrix to store circle data
 
             /* edge detection if necessary
-            myProc.cvtColor(myMat, edgeMat, Imgproc.COLOR_RGB2GRAY, 0);
+            myProc.cvtColor(imgMat, edgeMat, Imgproc.COLOR_RGB2GRAY, 0);
 
             myProc.Canny(edgeMat, edgeMat, 80, 100);*/
 
-            myProc.HoughCircles(myMat, CMat, Imgproc.CV_HOUGH_GRADIENT, dp, minDst);
+            myProc.HoughCircles(imgMat, CMat, Imgproc.CV_HOUGH_GRADIENT, dp, minDst); //find circles
 
             telemetry.addData("Num of Circles:", CMat.cols()); //columns determine num of circles
             telemetry.update();
